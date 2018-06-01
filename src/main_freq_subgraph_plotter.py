1fc007b1e3407a87d35701b933859df257bf7ed8
Dario Garcia-Gasulla <dario.garcia@bsc.es>
2018-06-02 00:40:06 +0200

    Working version

--- src/main_freq_subgraph_plotter.py
+++ src/main_freq_subgraph_plotter.py
@@ -1,24 +1,22 @@
 import numpy as np
-from graphnx import printGraph
+import networkx as nx
 
 def generateNXGraphLite(adj_mat):
-    import networkx as nx
     x = nx.from_numpy_matrix(adj_mat)
     return x
+
 file_location = "../results/gspan_1K_frequent_subgraphs_old.txt"
-
-
 with open(file_location) as f:
+    graphs = []
+    supports = []
     for l in f:
         #Skip empty lines
         if l == '\n': 
             continue
-        #If its a new graph. Print the current one and initialize the next
+        #If its a new graph. Store the current one and initialize the next
         if l[0] == '-':
-            #TODO print graph
-            g = generateNXGraphLite(adj_mat)
-            g_plot = printGraph(g, visualize = False)
-
+            graphs.append(generateNXGraphLite(adj_mat))
+            supports.append(support)
         if l[0] == 't':
             #Initialize next graph
             support = 0
@@ -26,15 +24,35 @@ with open(file_location) as f:
             vertices = []
         #Its a new vertex. Add it.
         if l[0] == 'v':
-            vertices.append((int(l.split(' ')[1]),int(l.split(' ')[2])))
+            vertices.append((int(l.split(' ')[1])))
         #Its a new edge. Add it.
         if l[0] == 'e':
             if first_edge:
                 adj_mat = np.zeros((len(vertices),len(vertices)))
                 first_edge = False
             #Add edge
-            adj_mat[int(l.split(' ')[1]),int(l.split(' ')[2])]
-        #TODO
+            adj_mat[int(l.split(' ')[1]),int(l.split(' ')[2])]+=1
         #Its the graph support. Store it.
         if l[0] == 'S':
             support = int(l.split(' ')[1])
+
+#Order graphs by support
+graphs = [x for _,x in sorted(zip(supports,graphs), reverse=True)]
+supports = sorted(supports, reverse=True)
+
+#Store all subgraphs in a file
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+with PdfPages('../results/frequent_subgraphs.pdf') as pdf:
+    for graph,support in zip(graphs,supports):
+        plt.figure(figsize=(7,7))
+        plt.axis('off')
+        edges,weights = zip(*nx.get_edge_attributes(graph,'weight').items())
+        pos = nx.spring_layout(graph,k=0.5,iterations=30)
+        nx.draw(graph, pos, edgelist=edges, edge_color=weights,
+                node_size = 700, width=5.0, edge_cmap=plt.cm.winter)
+        #nx.draw_networkx_labels(graph, pos, labels=labels, font_size = 20)
+        plt.title('Support: '+str(support))
+        pdf.savefig()
+        plt.close()
+
